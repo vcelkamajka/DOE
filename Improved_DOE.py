e4f2b05b7b8bb924f65f76bc7b10cc6d78e7431f
vcelkamajka <maja.rygolova@gmail.com>
2026-08-29 01:02:01 +0100

    Update and rename Improved DOE to Improved_DOE.py

--- Improved_DOE.py
+++ Improved_DOE.py
@@ -11,8 +11,11 @@ from statsmodels.stats.outliers_influence import variance_inflation_factor
 from scipy.stats import f_oneway
 from scipy.stats import shapiro
 from matplotlib.ticker import FormatStrFormatter
-import statsmodels.formula.api as smf
+from statsmodels.formula.api import ols
+import re
 
+def clean_col(col):
+    return re.sub(r'\W+', '_', col).strip('_')
 
 # full factorial: all combinations
 # fractional factorial: aliased factors, less runs -> less accurate, removes high level interactions
@@ -62,6 +65,18 @@ class DOE:
 
         print(self.df)
         print('=' * 50)
+
+    def stats(self):
+
+        temp = self.df.copy()
+        temp = self.df.rename(columns={col: clean_col(col) for col in temp.columns})
+        temp_y_col = clean_col(self.y_col)
+        temp_x_cols = [clean_col(c) for c in self.x_cols]
+
+        formula = f"{temp_y_col} ~ {' + '.join(temp_x_cols)}"
+        anova_model = ols(formula, data=temp).fit()
+        anova_table = sm.stats.anova_lm(anova_model, typ=2)
+        print(anova_table)
 
     def add_interactions(self):
         # e.g., if you have factors A, B, C -> need to construct factors AB, AC, BC and ABC
@@ -221,8 +236,10 @@ class DOE:
             f'{alpha / 2} CI': np.round(ci[0], 4),
             f'{1 - alpha / 2} CI': np.round(ci[1], 4),
         })
+
         results_df['Significant'] = results_df['P-value'] < alpha
         results_df = results_df.sort_values(by='Coefficient', ascending=False)
+        print('Results dataframe:')
         print(results_df)
 
         results_df.to_csv('results.csv')
@@ -235,7 +252,8 @@ class DOE:
                 colors.append('#f5424e')
 
         fig, ax = plt.subplots(figsize=(10, 10))
-        plt.bar(results_df['Feature'], results_df['Coefficient'], color=colors)
+        temp = results_df.drop('const')
+        plt.bar(temp['Feature'], temp['Coefficient'], color=colors)
         plt.xlabel('Features')
         plt.ylabel('Coefficients')
         plt.axhline(y=0, color='k', lw=0.5)
@@ -263,13 +281,14 @@ class DOE:
         self.results_df = results_df
         return self.results_df
 
-    def eliminate_factors(self, remaining_factors=3):
+    def eliminate_factors(self, sig_factors=None,remaining_factors=3):
         self.remaining_factors = remaining_factors
+        self.sig_factors = sig_factors
         # eliminate factors to a user defined level - default is 3
 
         sig_factors = self.results_df.loc[self.results_df['Significant'] == True, 'Feature']
         sig_factors = sig_factors.drop('const')
-        print(sig_factors)
+        display(sig_factors)
 
         if len(sig_factors) > remaining_factors:
             print(f'Excess factors remain - need to remove {len(sig_factors) - remaining_factors}!')
@@ -279,23 +298,39 @@ class DOE:
         selection_list = []
         for features in sig_factors:
             selection = self.results_df.loc[self.results_df['Feature'] == features]
+            selection_list.append(selection)
             display(selection)
 
-        to_drop = []
-        for n in range(len(sig_factors) - remaining_factors):
-            drop = input(
-                f"Based on the results shown below, please drop {len(sig_factors) - remaining_factors} factors by inputting the axis value, e.g., x1 followed by x2... ")
-            to_drop.append(drop)
+        temp = self.results_df
+        temp = temp.drop('const')
+        temp['Coefficient'] = np.abs(temp['Coefficient'])
 
-        #sig_factors = pd.DataFrame()
-        for drop in to_drop:
-            sig_factors = sig_factors.drop(drop)
+        manual_query = input('Do you wish to automatically eliminate factors based on ascending coefficient value? (Y/N): ')
 
-        print('Remaining features:')
-        print(sig_factors)
+        if manual_query == 'N':
+            count = 0
+            to_drop = []
+            for n in range(len(sig_factors) - remaining_factors):
+                drop = input(
+                    f"Based on the results shown above, please drop {len(sig_factors) - remaining_factors + count} factors by inputting the axis value, e.g., x1, click enter and if required input another afterwards e.g., x2... ")
+                count -= 1
+                to_drop.append(drop)
+
+            for drop in to_drop:
+                sig_factors = sig_factors.drop(drop)
+            print('Remaining features:')
+            display(sig_factors)
+            self.sig_factors = sig_factors
+
+        if manual_query == 'Y':
+            temp = temp.sort_values(by='Coefficient', ascending=False)
+            index_range = len(sig_factors) - (remaining_factors+1)
+            temp.drop(temp.index[index_range:], inplace=True)
+            print(temp)
+            sig_factors = temp['Feature']
+            self.sig_factors = sig_factors
 
         print('=' * 50)
-        self.sig_factors = sig_factors
 
         x_reduced = self.x_poly_df[sig_factors]
 
@@ -306,18 +341,47 @@ class DOE:
         print(f'Full model R²: {self.osl_model.rsquared:.2f}, Adj R²: {self.osl_model.rsquared_adj:.3f}')
         print(f'Reduced model R²: {reduced_model.rsquared:.2f}, Adj R²: {reduced_model.rsquared_adj:.3f}')
 
+        with open("Adjusted_OLS_summary.csv", "w") as f:
+            f.write(self.osl_model.summary().as_csv())
 
-        return self.sig_factors
+        self.x_cols = []
+        for n in self.sig_factors:
+            self.x_cols.append(n)
+
 
     def RSM(self):
-        pass
+
+        x_coefs_df = pd.DataFrame()
+        x_coefs_df = self.results_df[self.results_df['Feature'].isin(self.x_cols)]
+        x_coefs_df = x_coefs_df.drop(['P-value','0.025 CI','0.975 CI', 'Significant'], axis='columns')
+        print(x_coefs_df)
+        temp = x_coefs_df
+
+        fig, ax = plt.subplots(figsize=(10, 10), kwargs={'projection':'3d'})
+
+        if self.remaining_factors == 3:
+
+            x = np.linspace()
+            y = np.linspace()
+            x,y = np.meshgrid(x,y)
+
+            z =
+
+            ax.plot_surface(x,y,z, cmap='inferno',antialiased=False)
+
+            plt.show()
+
+
+
 
 # need to construct 2D &/or 3D RSM of results and maximise/minimise
 
-t = DOE('test.csv')
+
+t = DOE('reaction_DOE.csv')
 t.add_interactions()
+t.stats()
 t.main_effects()
 t.interaction_plots()
 t.get_trend(2)
-t.eliminate_factors()
+t.eliminate_factors(remaining_factors=3)
 t.RSM()
